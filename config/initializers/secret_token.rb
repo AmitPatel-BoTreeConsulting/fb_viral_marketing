06b80660cc2fa6da21fd727f94ab3d54fb5ee2b4
AmitPatel-BoTreeConsulting <amit.patel@botreeconsulting.com>
2013-01-06 11:32:40 +0530

    Initial push with devise and bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FbViralMarketing::Application.config.secret_token = '8782e3abd68fa6e300a1524a401f1deff1809b3d286c260fb64d61b2685f368f29eda7352fd196aae9f95254ac3efea02f8df91cfd1f8c7892ee05f641064769'
